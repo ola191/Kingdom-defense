728d788306575db7dfe82b783f2f454d7dd3355f
Ola Krassowska <ola.krassowska@hotmail.com>
2024-10-09 22:50:10 +0200

    Rebuilding the levels scene code, changing the structure using the class and cleaning the file

--- scenes/levels.py
+++ scenes/levels.py
@@ -8,64 +8,65 @@ from ui.filters.brightness import ui_brightness
 from ui.layout.column import layout_column
 
 
-def scene_levels(screen):
-    running = True
+class SceneLevels:
+    def __init__(self, screen):
+        self.screen = screen
+        self.width, self.height = screen.get_size()
 
-    levels = []
+        self.levels = []
 
-    # mC ~ mainConfig
-    with open("data/config.json", "r") as mC:
-        data = json.load(mC)
-        for level in data["levels"]:
-            levels.append(level['title'])
+        # mC ~ mainConfig
+        with open("data/config.json", "r") as mC:
+            data = json.load(mC)
+            self.levels = [level["title"] for level in data["levels"]]
+            self.brightness_from_config = data["settings"]["brightness"]
 
-    levels_buttons = []
+        self.background_image = pygame.image.load("images/backgrounds/levels.png")
+        self.background_image = pygame.transform.scale(self.background_image, (self.width, self.height))
+        self.filter = ui_brightness(self.screen, self.brightness_from_config)
 
-    width, height = screen.get_size()
+        self.levels_buttons = [
+            ui_button(self.screen, level, (0,0), (300,50), None, "button_vertical.png") for level in self.levels
+        ]
 
-    background_image = pygame.image.load("images/backgrounds/levels.png")
-    background_image = pygame.transform.scale(background_image, (width, height))
+        self.back_button = ui_button(screen, "back to main menu", (0, 0), (300, 50),  None, "button_vertical.png")
 
-    with open("data/config.json", "r") as mC:
-        data = json.load(mC)
-        brightness_from_config = data["settings"]["brightness"]
+        self.buttons_to_layout = self.levels_buttons + [self.back_button]
+        self.spacing = 20
+        self.positions = layout_column(self.buttons_to_layout, self.width, self.height, self.spacing)
 
-    filter = ui_brightness(screen, brightness_from_config)
+    def draw(self):
+        self.screen.blit(self.background_image, (0,0))
 
-    while running:
-        screen.blit(background_image, (0,0))
-
-        buttons_to_layout = []
-
-        for level in levels:
-            button_rect = ui_button(screen, level, (0, 0), (300, 50),  None, "button_vertical.png")
-            levels_buttons.append((button_rect, level))
-            buttons_to_layout.append(button_rect)
-
-        back_button = ui_button(screen, "back to main menu", (0, 0), (300, 50),  None, "button_vertical.png")
-
-        buttons_to_layout.append(back_button)
-
-        spacing = 20
-        available_width, available_height = screen.get_size()
-        positions = layout_column(buttons_to_layout, available_width, available_height, spacing)
-
-        for button, (x, y) in zip(buttons_to_layout, positions):
+        for button, (x, y) in zip(self.buttons_to_layout, self.positions):
             button.position = (x, y)
             button.draw()
 
-        filter.draw()
+        self.filter.draw()
 
-        pygame.display.flip()
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.back_button.collidepoint(event.pos):
+                return 'main_menu'
+            for button, level in zip(self.levels_buttons, self.levels):
+                if button.collidepoint(event.pos):
+                    return f'game_{level}'
 
+def scene_levels(screen):
+    levels_scene = SceneLevels(screen)
+    running = True
+
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                if back_button.collidepoint(event.pos):
-                    return 'main_menu'
-                for button_rect, level in levels_buttons:
-                    if button_rect.collidepoint(event.pos):
-                        return f'game_{level}'
+            else:
+                scene_action = levels_scene.handle_event(event)
+                if scene_action:
+                    return scene_action
+
+        levels_scene.draw()
+
+        pygame.display.flip()
 
     pygame.quit()
