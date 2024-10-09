b8c7eef67c254846a1e5b71c0f8bb40f7dbc6385
Ola Krassowska <ola.krassowska@hotmail.com>
2024-10-09 18:22:02 +0200

    Added levels from config in levels scene as buttons and added in config two new levels town and castle?

--- scenes/levels.py
+++ scenes/levels.py
@@ -1,3 +1,5 @@
+import json
+
 import pygame
 import sys
 
@@ -5,10 +7,26 @@ from ui.components.button import ui_button
 
 def scene_levels(screen):
     running = True
+
+    levels = []
+
+    # mC ~ mainConfig
+    with open("data/config.json", "r") as mC:
+        data = json.load(mC)
+        print(data)
+        for level in data["levels"]:
+            levels.append(level['title'])
+
     while running:
         screen.fill((0, 0, 0))
 
-        back_button = ui_button(screen, "back to main menu", (300, 200), (200, 50))
+        actual_y = 100
+
+        for level in levels:
+            ui_button(screen, level, (300, actual_y), (200, 50))
+            actual_y += 70
+
+        back_button = ui_button(screen, "back to main menu", (300, actual_y), (200, 50))
 
         pygame.display.flip()
 
