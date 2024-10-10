bb6a530c20e2f008ca29b506653795b9f955c1b0
Ola Krassowska <ola.krassowska@hotmail.com>
2024-10-10 13:20:41 +0200

    Added navigator to levels

--- scenes/levels.py
+++ scenes/levels.py
@@ -3,10 +3,12 @@ import json
 import pygame
 import sys
 
+from ui.animations import Animation
 from ui.components.button import ui_button
 from ui.filters.brightness import ui_brightness
 from ui.layout.column import layout_column
 from ui.layout.navbar import layout_navbar
+from ui.navigators.button_navigator import Navigator
 
 
 class SceneLevels:
@@ -27,13 +29,15 @@ class SceneLevels:
         self.filter = ui_brightness(self.screen, self.brightness_from_config)
 
         self.levels_buttons = [
-            ui_button(self.screen, level, (0,0), (300,50), None, "button_vertical.png") for level in self.levels
+            ui_button(self.screen, level, (0,0), (300,50), None, "button_vertical.png", 100) for level in self.levels
         ]
+
+        self.levels_buttons[0].brightness = 80
 
         self.navbar_buttons = []
 
-        self.library_button = ui_button(screen, "library", (self.width - 250, 25), (200, 50), None, "button_vertical.png")
-        self.back_button = ui_button(screen, "back", (0, 0), (75, 50),  None, "button_vertical.png")
+        self.library_button = ui_button(screen, "library", (self.width - 250, 25), (200, 50), None, "button_vertical.png", 100)
+        self.back_button = ui_button(screen, "back", (0, 0), (75, 50),  None, "button_vertical.png", 100)
 
         self.navbar_buttons[:] = [*self.navbar_buttons, self.library_button, self.back_button]
 
@@ -41,8 +45,21 @@ class SceneLevels:
 
         # self.buttons_to_layout = self.levels_buttons + [self.back_button]
         self.buttons_to_layout = self.levels_buttons
+
+        self.buttons_to_navigation = self.levels_buttons + self.navbar_buttons
+        self.button_navigator = Navigator(self.buttons_to_navigation)
+
         self.spacing = 20
         self.positions = layout_column(self.buttons_to_layout, self.width, self.height, self.spacing)
+
+        self.selected_button = self.levels_buttons[0]
+
+        self.mixer = pygame.mixer
+        self.mixer.init()
+
+        self.click_sound = self.mixer.Sound("sounds/button_click.wav")
+
+        self.button_navigator.update_selection()
 
     def draw(self):
         self.screen.blit(self.background_image, (0,0))
@@ -65,10 +82,33 @@ class SceneLevels:
                 return 'library'
             for button, level in zip(self.levels_buttons, self.levels):
                 if button.collidepoint(event.pos):
+                    Animation.click_background(self.screen, 80, 40, 30, button, 200)
                     return f'game_{level}'
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 return 'main_menu'
+            elif event.key == pygame.K_DOWN:
+                self.click_sound.play()
+
+                self.button_navigator.next()
+                self.selected_button = self.button_navigator.get_current()
+            elif event.key == pygame.K_UP:
+                self.click_sound.play()
+
+                self.button_navigator.previous()
+                self.selected_button = self.button_navigator.get_current()
+            elif event.key == pygame.K_RETURN:
+                self.click_sound.play()
+
+                current_button =self.button_navigator.get_current()
+                if current_button == self.back_button:
+                    return 'main_menu'
+                elif current_button == self.library_button:
+                    return 'library'
+                else:
+                    level_name = self.levels[self.button_navigator.current_index]
+                    Animation.click_background(self.screen, 80, 40, 30, current_button, 200)
+                    return f"game_{level_name}"
 
 def scene_levels(screen):
     levels_scene = SceneLevels(screen)
