46be596ace7111a8af364ba60e68916a5eacbb62
Ola Krassowska <ola.krassowska@hotmail.com>
2024-10-10 11:16:38 +0200

    Added brightness as selected button effect to button ui, and key down key up menu system moving with enter event to choose and button_navigator class to manage it

--- scenes/main_menu.py
+++ scenes/main_menu.py
@@ -24,25 +24,28 @@ class SceneMainMenu:
         self.screen = screen
         self.width, self.height = self.screen.get_size()
 
-
         self.background_image = pygame.image.load("images/backgrounds/mainmenu.jpg")
         self.background_image = pygame.transform.scale(self.background_image, (self.width, self.height))
-
+        self.selected_button = None
         with open("data/config.json", "r") as mC:
             data = json.load(mC)
             self.brightness_from_config = data["settings"]["brightness"]
 
         self.filter = ui_brightness(screen, self.brightness_from_config)
 
+        self.start_button = ui_button(screen, "Start Game", (0, 0), (300, 50), None, "button_vertical.png", 80)
+        self.settings_button = ui_button(screen, "Settings", (0, 0), (300, 50),  None, "button_vertical.png", 100)
+        self.quit_button = ui_button(screen, "Close Game", (0, 0), (300, 50),  None, "button_vertical.png", 100)
 
-        self.start_button = ui_button(screen, "Start Game", (0, 0), (300, 50), None, "button_vertical.png")
-        self.settings_button = ui_button(screen, "Settings", (0, 0), (300, 50),  None, "button_vertical.png")
-        self.quit_button = ui_button(screen, "Close Game", (0, 0), (300, 50),  None, "button_vertical.png")
+        self.selected_button = self.start_button
 
         self.buttons_to_layout = [self.start_button, self.settings_button, self.quit_button]
+
         self.spacing = 20
         self.available_width, self.available_height = self.screen.get_size()
         self.positions = layout_column(self.buttons_to_layout, self.available_width, self.available_height, self.spacing)
+
+        self.button_navigator = ButtonNavigator(self.buttons_to_layout)
 
     def draw(self):
         self.screen.blit(self.background_image, (0,0))
@@ -62,11 +65,62 @@ class SceneMainMenu:
                 return 'settings'
             elif self.quit_button.collidepoint(event.pos):
                 self.quit_game()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                self.button_navigator.next()
+                self.buttons_to_layout = self.button_navigator.get_buttons()
+                self.selected_button = self.button_navigator.get_current()
+            elif event.key == pygame.K_UP:
+                self.button_navigator.previous()
+                self.buttons_to_layout = self.button_navigator.get_buttons()
+                self.selected_button = self.button_navigator.get_current()
+            elif event.key == pygame.K_RIGHT or event.key == pygame.K_RETURN:
+                if self.selected_button == self.start_button:
+                    return 'levels'
+                elif self.selected_button == self.settings_button:
+                    return 'settings'
+                elif self.selected_button == self.quit_button:
+                    quit_game()
+
+
 
     @staticmethod
     def quit_game():
         pygame.quit()
         sys.exit()
+
+class ButtonNavigator:
+    def __init__(self, buttons):
+        self.buttons = buttons
+        self.current_index = 0
+        self.brightness_delta = 20
+        self.old_index = self.current_index
+
+    def get_current(self):
+        return self.buttons[self.current_index]
+
+    def get_buttons(self):
+        return self.buttons
+
+    def next(self):
+        self.current_index = (self.current_index + 1) % len(self.buttons)
+        self.change_selected_button_brightness()
+
+    def previous(self):
+        self.current_index = (self.current_index - 1) % len(self.buttons)
+        self.change_selected_button_brightness()
+
+    def change_selected_button_brightness(self):
+        button_to_normal = self.buttons[self.old_index]
+        current_brightness = button_to_normal.brightness
+        button_to_normal.change_brightness(current_brightness + self.brightness_delta)
+
+        self.old_index = self.current_index
+
+        button_to_change = self.buttons[self.current_index]
+        current_brightness = button_to_change.brightness
+        button_to_change.change_brightness(current_brightness - self.brightness_delta)
+
 
 def scene_main_menu(screen):
     main_menu_scene = SceneMainMenu(screen)
--- ui/components/button.py
+++ ui/components/button.py
@@ -3,13 +3,14 @@ import pygame
 from ui.colors import *
 
 class ui_button:
-    def __init__(self, screen, text, position, size, action=None, image=None):
+    def __init__(self, screen, text, position, size, action=None, image=None, brightness=100):
         self.screen = screen
         self.text = text
         self.rect = pygame.Rect(position, size)
         self.position = position
         self.size = size
         self.image = image
+        self.brightness = brightness
 
     def draw(self):
         font = pygame.font.Font(None, 40)
@@ -26,9 +27,17 @@ class ui_button:
             pygame.draw.rect(self.screen, ui_color_white, button_rect)
         label = font.render(self.text, True, ui_color_black)
         self.screen.blit(label, (self.position[0] + (self.size[0] - label.get_width()) // 2, self.position[1] + (self.size[1] - label.get_height()) // 2))
+        if self.brightness is not 100:
+            self.brightness_filter = pygame.Surface(self.get_size())
+            self.brightness_filter.fill(ui_color_black)
+            self.brightness_filter.set_alpha(255 - int(255 * (self.brightness / 100)))
+            self.screen.blit(self.brightness_filter, self.position)
 
     def get_size(self):
         return self.rect.size
+
+    def change_brightness(self, brightness):
+        self.brightness = brightness
 
     def collidepoint(self, pos):
         return self.rect.collidepoint(pos)
