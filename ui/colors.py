06df052070817c18acd26e97a6a657bb149614f4
Ola Krassowska <ola.krassowska@hotmail.com>
2024-10-11 21:31:39 +0200

    new color

--- ui/colors.py
+++ ui/colors.py
@@ -1,8 +1,11 @@
 ui_color_white = (255, 255, 255)
 ui_color_black = (0, 0, 0)
-ui_color_blue = (0, 0, 255)
+
 ui_color_red = (255, 0, 0)
 ui_color_green = (0, 255, 0)
+ui_color_blue = (0, 0, 255)
+ui_color_yellow = (255, 255, 0)
+
 ui_color_sand = (242, 210, 169)
 ui_color_grass = (50, 142, 31)
 ui_color_tower = (100, 80, 90)
