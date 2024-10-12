a9aa756267b5f221408b648811877b9b43035ecf
Ola Krassowska <ola.krassowska@hotmail.com>
2024-10-12 22:02:00 +0200

    new names for textures colors

--- ui/colors.py
+++ ui/colors.py
@@ -7,5 +7,5 @@ ui_color_blue = (0, 0, 255)
 ui_color_yellow = (255, 255, 0)
 
 ui_color_sand = (242, 210, 169)
-ui_color_grass = (50, 142, 31)
+ui_color_grass_100 = (110 ,137 ,46)
 ui_color_tower = (100, 80, 90)
