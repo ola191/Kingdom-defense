9f69a932dbefed18d60fa9f8e020e30be1335ef6
Ola Krassowska <ola.krassowska@hotmail.com>
2024-10-13 23:30:46 +0200

    Part of changing all logic of enemies and towers 3/3 (start version not dev like dev), goblins are moving with animation, animation is loading only one time for fps v2

---
+++ Game/animations.py
@@ -0,0 +1,15 @@
+import pygame
+
+
+def load_animation(animation_name, block_unit):
+    if animation_name == "goblin":
+        animation_frames = []
+        for i in range(3):
+            tileset_image = pygame.image.load(f"images/monsters/sprite_{i}.png").convert_alpha()
+            tileset_image = pygame.transform.scale(tileset_image, (block_unit * 9, block_unit * 9))
+
+            rect = pygame.Rect(block_unit * 6, block_unit * 6, block_unit * 3, block_unit * 3)
+
+            animation_frames.append(tileset_image.subsurface(rect))
+
+        return animation_frames
